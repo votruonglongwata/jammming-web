fdcf3ba4346f74e3316371a7f1187e53bee08afd
votruonglong <long.vo@watatek.com>
2025-06-18 16:45:30 +0700

    login

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,54 @@ import SearchBar from './components/search-bar'
 import SearchResults from './components/search-result'
 function App() {
 
-  const clientId = '9cadc247ee634265b68c5d118ac46480';
-  const redirectUri = 'https://jammming-web.vercel.app';
-  const scopes = [
-    'playlist-modify-public',
-    'playlist-modify-private',
-    'user-read-private',
-  ];
+  // Utility to generate code challenge from verifier
+  function generateCodeChallenge(verifier) {
+    return crypto.subtle.digest('SHA-256', new TextEncoder().encode(verifier))
+      .then(hash => {
+        return btoa(String.fromCharCode(...new Uint8Array(hash)))
+          .replace(/\+/g, '-')
+          .replace(/\//g, '_')
+          .replace(/=+$/, '');
+      });
+  }
+
+  // Generate random string
+  function generateRandomString(length) {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+  }
+
+  async function redirectToSpotifyLogin() {
+    const clientId = '9cadc247ee634265b68c5d118ac46480';
+    const redirectUri = 'https://jammming-web.vercel.app';
+    const scopes = [
+      'playlist-modify-public',
+      'playlist-modify-private',
+      'user-read-private'
+    ];
+
+    const codeVerifier = generateRandomString(128);
+    const codeChallenge = await generateCodeChallenge(codeVerifier);
+
+    localStorage.setItem('code_verifier', codeVerifier);
+    console.log(codeVerifier);
+
+
+    const authUrl = new URL('https://accounts.spotify.com/authorize');
+    authUrl.searchParams.set('response_type', 'code');
+    authUrl.searchParams.set('client_id', clientId);
+    authUrl.searchParams.set('scope', scopes.join(' '));
+    authUrl.searchParams.set('redirect_uri', redirectUri);
+    authUrl.searchParams.set('code_challenge_method', 'S256');
+    authUrl.searchParams.set('code_challenge', codeChallenge);
+
+    window.location.href = authUrl.toString();
+  }
+
 
 
 
@@ -27,7 +68,7 @@ function App() {
     <div>
       <div className='header'>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
-        <button type='button' className='login-button' onClick={handleLogin}>
+        <button type='button' className='login-button' onClick={redirectToSpotifyLogin}>
           Login with Spotify
         </button>
       </div>
