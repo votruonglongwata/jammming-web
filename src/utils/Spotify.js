f8b7c6c526808d00630502d671cb85507b975c2b
votruonglong <long.vo@watatek.com>
2025-06-19 09:11:23 +0700

    Update Spotify.js

--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -4,16 +4,23 @@ const clientId = '9cadc247ee634265b68c5d118ac46480';
 const redirectUri = 'https://jammming-web.vercel.app';
 const tokenEndpoint = 'https://accounts.spotify.com/api/token';
 
-
+let tokenExpirationTime = 0;
 let accessToken = '';
 
 const Spotify = {
     async getAccessToken() {
-        if (accessToken) return accessToken;
+        const currentTime = Date.now();
+
+        if (accessToken && currentTime < tokenExpirationTime) {
+            return accessToken;
+        }
 
         const storedToken = localStorage.getItem('access_token');
-        if (storedToken) {
+        const storedExpiration = localStorage.getItem('token_expiration');
+
+        if (storedToken && storedExpiration && currentTime < parseInt(storedExpiration, 10)) {
             accessToken = storedToken;
+            tokenExpirationTime = parseInt(storedExpiration, 10);
             return accessToken;
         }
 
